c5d5af1c993e7458318a4fc313e434591efafe27
JunweiNotAvailable <wertycxx@gmail.com>
2025-04-18 19:38:07 +0800

    updated env

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,7 +2,7 @@ const env = process.env as any;
 
 export const Config = {
   api: {
-    url: env.EXPO_PUBLIC_ENV === 'production' ? env.EXPO_PUBLIC_API_URL : 'http://172.20.10.3:3000',
+    url: env.EXPO_PUBLIC_ENV === 'development' ? 'http://172.20.10.3:3000' : 'https://review-my-campus-backend-production.up.railway.app',
   },
   storage: {
     user: 'current_user',
